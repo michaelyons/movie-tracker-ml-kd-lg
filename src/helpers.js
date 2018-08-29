5f99ab57bdcd428ce1a7d3a3b3f5a13674a07220
Kiel Dehnert <kielzor@gmail.com>
2018-08-29 17:08:50 -0600

    Added images to new helper file, sorry LeeLee, didn't see you had changed that file and accidently deleted it to clean up file tree. Should be all good now

--- src/helpers.js
+++ src/helpers.js
@@ -13,6 +13,7 @@ export const cleanMovies = async data => {
       title: result.title,
       overview: result.overview,
       rating: result.vote_average,
+      image: `https://image.tmdb.org/t/p/w200${result.poster_path}`,
       id: (Date.now() * Math.random()).toFixed(0)
     };
   });

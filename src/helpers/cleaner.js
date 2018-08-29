ce0666b9d92a1ce505f8384a9541b6b6a20c20c8
Lee Graham <graham.la3@gmail.com>
2018-08-29 14:13:40 -0600

    Refactor result.id to be dynamic and match the id of the original object in the api

--- src/helpers/cleaner.js
+++ src/helpers/cleaner.js
@@ -4,7 +4,7 @@ export const cleanMovies = async data => {
       title: result.title,
       overview: result.overview,
       rating: result.vote_average,
-      id: (Date.now() * Math.random()).toFixed(0)
+      id: result.id
     };
   });
 };

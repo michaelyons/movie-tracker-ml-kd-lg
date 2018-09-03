2eafd07d4d18b9538e086eba75b236927c79e059
Kiel Dehnert <kielzor@gmail.com>
2018-09-03 14:10:36 -0600

    Test displayMovieCardsReducer

--- src/reducers/addFavoriteMovieReducer.test.js
+++ src/reducers/addFavoriteMovieReducer.test.js
@@ -1,5 +1,4 @@
 import { addFavoriteMovieReducer } from './addFavoriteMovieReducer'
-import { addFavorite } from '../helpers';
 
 describe('addFavoriteMovieReducer', () => {
   it('should return the initial state', () => {
@@ -12,30 +11,30 @@ describe('addFavoriteMovieReducer', () => {
 
   it('should return state with a favorite added', () => {
     const mockState = []
-    const action = { type: 'ADD_FAVORITE', favorite: {title: 'Mission Impossible'}}
+    const mockAction = { type: 'ADD_FAVORITE', favorite: {title: 'Mission Impossible'}}
     const expected = [{ title: 'Mission Impossible'}]
 
-    const result = addFavoriteMovieReducer(mockState, action)
+    const result = addFavoriteMovieReducer(mockState, mockAction)
 
     expect(result).toEqual(expected)
   })
 
   it('should reset favorites', () => {
     const mockState = [{ title: 'Mission Impossible' }]
-    const action = { type: 'RESET_FAVORITE', favorite: { data: [] }}
+    const mockAction = { type: 'RESET_FAVORITE', favorite: { data: [] }}
     const expected = []
 
-    const result = addFavoriteMovieReducer(mockState, action)
+    const result = addFavoriteMovieReducer(mockState, mockAction)
 
     expect(result).toEqual(expected)
   })
 
   it('should delete a favorite', () => {
     const mockState = [{ title: 'Mission Impossible' }, { title: 'Scooby Doo' }]
-    const action = { type: 'DELETE_FAVORITE', favorite: { title: 'Mission Impossible' }}
+    const mockAction = { type: 'DELETE_FAVORITE', favorite: { title: 'Mission Impossible' }}
     const expected = [{ title: 'Scooby Doo' }]
 
-    const result = addFavoriteMovieReducer(mockState, action)
+    const result = addFavoriteMovieReducer(mockState, mockAction)
 
     expect(result).toEqual(expected)
   })

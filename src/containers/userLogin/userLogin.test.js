0b31ba17f47ec7271aebe13260d8fb96c329100f
Lee Graham <graham.la3@gmail.com>
2018-09-04 06:54:49 -0600

    Setup initial test structure for login control form field tests.

---
+++ src/containers/userLogin/userLogin.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import UserLogin from './UserLogin'
+import { shallow } from 'enzyme'
+
+describe('UserLogin', () => {
+  let wrapper
+  let mockHandleLogin = jest.fn()
+  let mockState = { email: '', password: ''}
+
+  beforeEach(() => {
+    wrapper = shallow(<UserLogin handleLogin={mockHandleLogin} />)
+  })
+
+
+})
